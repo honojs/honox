c193c72b784ed7a7862a3f01e249eb25889224a6
Yusuke Wada <yusuke@kamawada.com>
2024-01-23 23:28:04 +0900

    fix(vite): simplify vite config

--- examples/basic/app/server.ts
+++ examples/basic/app/server.ts
@@ -1,10 +1,7 @@
-import { serveStatic } from 'hono/cloudflare-pages'
 import { showRoutes } from 'hono/dev'
 import { createApp } from 'honox/server'
 
-const app = createApp({
-  init: (app) => app.get('/static/*', serveStatic()),
-})
+const app = createApp()
 
 showRoutes(app)
 
--- examples/mdx/vite.config.ts
+++ examples/mdx/vite.config.ts
@@ -1,6 +1,6 @@
 import devServer from '@hono/vite-dev-server'
 import mdx from '@mdx-js/rollup'
-import { islandComponents } from 'honox/vite'
+import honox from 'honox/vite'
 import remarkFrontmatter from 'remark-frontmatter'
 import remarkMdxFrontmatter from 'remark-mdx-frontmatter'
 import { defineConfig } from '../../node_modules/vite'
@@ -11,7 +11,7 @@ const entry = './app/server.ts'
 export default defineConfig(() => {
   return {
     plugins: [
-      islandComponents(),
+      honox(),
       devServer({ entry }),
       ssg({ entry }),
       mdx({
