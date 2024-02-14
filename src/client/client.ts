ff124380659fe0ce0e5dccbee499d108cf84ca15
Taku Amano <taku@taaas.jp>
2024-02-14 18:03:59 +0900

    feat: re-create children from rendered template element

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,7 +1,7 @@
-import { render } from 'hono/jsx/dom'
+import { render, Suspense, use } from 'hono/jsx/dom'
 import { jsx as jsxFn } from 'hono/jsx/dom/jsx-runtime'
-import { COMPONENT_NAME, DATA_SERIALIZED_PROPS } from '../constants.js'
-import type { CreateElement, Hydrate } from '../types.js'
+import { COMPONENT_NAME, DATA_HONO_TEMPLATE, DATA_SERIALIZED_PROPS } from '../constants.js'
+import type { CreateElement, CreateChildren, Hydrate } from '../types.js'
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 type FileCallback = () => Promise<{ default: Promise<any> }>
@@ -9,8 +9,112 @@ type FileCallback = () => Promise<{ default: Promise<any> }>
 export type ClientOptions = {
   hydrate?: Hydrate
   createElement?: CreateElement
+  /**
+   * Create "children" attribute of a component from a list of child nodes
+   */
+  createChildren?: CreateChildren
   ISLAND_FILES?: Record<string, () => Promise<unknown>>
   island_root?: string
+}
+
+const buildCreateChildrenFn = (createElement: CreateElement): CreateChildren => {
+  const createChildren = (childNodes: NodeListOf<ChildNode>): Node[] => {
+    const children = []
+    for (let i = 0; i < childNodes.length; i++) {
+      const child = childNodes[i] as HTMLElement
+      if (child.nodeType === 8) {
+        // skip comments
+        continue
+      } else if (child.nodeType === 3) {
+        // text node
+        children.push(child.textContent)
+      } else if (child.nodeName === 'TEMPLATE' && child.id.match(/(?:H|E):\d+/)) {
+        const placeholderElement = document.createElement('hono-placeholder')
+        placeholderElement.style.display = 'none'
+
+        let resolve: (nodes: Node[]) => void
+        const promise = new Promise<Node[]>((r) => (resolve = r))
+
+        // Suspense: replace content by `replaceWith` when resolved
+        // ErrorBoundary: replace content by `replaceWith` when error
+        child.replaceWith = (node: DocumentFragment) => {
+          resolve(createChildren(node.childNodes))
+          placeholderElement.remove()
+        }
+
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        let fallback: any = []
+
+        // gather fallback content and find placeholder comment
+        for (
+          // equivalent to i++
+          placeholderElement.appendChild(child);
+          i < childNodes.length;
+          i++
+        ) {
+          const child = childNodes[i]
+          if (child.nodeType === 8) {
+            // <!--/$--> or <!--E:1-->
+            placeholderElement.appendChild(child)
+            i--
+            break
+          } else if (child.nodeType === 3) {
+            fallback.push(child.textContent)
+          } else {
+            fallback.push(
+              createElement(child.nodeName, {
+                children: createChildren(child.childNodes),
+              })
+            )
+          }
+        }
+
+        // if already resolved or error, get content from added template element
+        const fallbackTemplates = document.querySelectorAll<HTMLTemplateElement>(
+          `[data-hono-target="${child.id}"]`
+        )
+        if (fallbackTemplates.length > 0) {
+          const fallbackTemplate = fallbackTemplates[fallbackTemplates.length - 1]
+          fallback = createChildren(fallbackTemplate.content.childNodes)
+        }
+
+        // if no content available, wait for ErrorBoundary fallback content
+        if (fallback.length === 0 && child.id.startsWith('E:')) {
+          let resolve: (nodes: Node[]) => void
+          const promise = new Promise<Node[]>((r) => (resolve = r))
+          fallback = createElement(Suspense, {
+            fallback: [],
+            children: [createElement(() => use(promise), {})],
+          })
+          placeholderElement.insertBefore = ((node: DocumentFragment) => {
+            resolve(createChildren(node.childNodes))
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          }) as any
+        }
+
+        // wait for content to be resolved by placeholderElement
+        document.body.appendChild(placeholderElement)
+
+        // render fallback content
+        children.push(
+          createElement(Suspense, {
+            fallback,
+            children: [createElement(() => use(promise), {})],
+          })
+        )
+      } else {
+        children.push(
+          createElement(child.nodeName, {
+            children: createChildren(child.childNodes),
+          })
+        )
+      }
+    }
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    return children as any
+  }
+
+  return createChildren
 }
 
 export const createClient = async (options?: ClientOptions) => {
@@ -32,6 +136,18 @@ export const createClient = async (options?: ClientOptions) => {
 
           const hydrate = options?.hydrate ?? render
           const createElement = options?.createElement ?? jsxFn
+
+          const maybeTemplate = element.childNodes[element.childNodes.length - 1]
+          if (
+            maybeTemplate?.nodeName === 'TEMPLATE' &&
+            (maybeTemplate as HTMLElement)?.attributes.getNamedItem(DATA_HONO_TEMPLATE) !== null
+          ) {
+            const createChildren =
+              options?.createChildren ?? buildCreateChildrenFn(createElement as CreateElement)
+            props.children = await createChildren(
+              (maybeTemplate as HTMLTemplateElement).content.childNodes
+            )
+          }
 
           const newElem = await createElement(Component, props)
           // @ts-expect-error default `render` cause a type error
--- src/types.ts
+++ src/types.ts
@@ -3,3 +3,4 @@
 /** JSX */
 export type CreateElement = (type: any, props: any) => Node | Promise<Node>
 export type Hydrate = (children: Node, parent: Element) => void | Promise<void>
+export type CreateChildren = (childNodes: NodeListOf<ChildNode>) => Node[] | Promise<Node[]>
