b41c89a5a1dd77c91bf3338e4a08e5027e8a55eb
Bruce Harrison <leeharrison1984@gmail.com>
2024-02-26 11:32:44 -0600

    condense logic

--- src/vite/inject-importing-islands.ts
+++ src/vite/inject-importing-islands.ts
@@ -10,51 +10,50 @@ const generate = (_generate.default as typeof _generate) ?? _generate
 
 export function injectImportingIslands(): Plugin {
   const visited = {}
-  const regex = new RegExp(/\\islands\\/)
+  const isIslandRegex = new RegExp(/\\islands\\/)
+  const fileExtensionRegex = new RegExp(/\.[t|j]sx$/)
 
   return {
     name: 'inject-importing-islands',
-    transform(code, id) {
-      if (id.endsWith('.tsx') || id.endsWith('.jsx')) {
-        const ast = parse(code, {
-          sourceType: 'module',
-          plugins: ['jsx'],
-        })
-
-        const hasIslandsImport = dependencyTree
-          .toList({
-            filename: id,
-            directory: '.',
-            visited,
-          })
-          .some((x) => regex.test(x))
-
-        if (hasIslandsImport) {
-          const hasIslandsNode: Statement = {
-            type: 'ExportNamedDeclaration',
-            specifiers: [],
-            declaration: {
-              type: 'VariableDeclaration',
-              declarations: [
-                {
-                  type: 'VariableDeclarator',
-                  id: { type: 'Identifier', name: IMPORTING_ISLANDS_ID },
-                  init: { type: 'BooleanLiteral', value: true },
-                },
-              ],
-              kind: 'const',
-            },
-          }
-          // eslint-disable-next-line @typescript-eslint/no-explicit-any
-          ast.program.body.push(hasIslandsNode)
-        }
-
-        const output = generate(ast, {}, code)
-        return {
-          code: output.code,
-          map: output.map,
-        }
+    transform(sourceCode, id) {
+      if (!fileExtensionRegex.test(id)) {
+        return
       }
+
+      const hasIslandsImport = dependencyTree
+        .toList({
+          filename: id,
+          directory: '.',
+          visited,
+        })
+        .some((x) => isIslandRegex.test(x))
+
+      if (!hasIslandsImport) {
+        return
+      }
+
+      const ast = parse(sourceCode, {
+        sourceType: 'module',
+        plugins: ['jsx'],
+      })
+      ast.program.body.push(hasIslandsNode)
+      return generate(ast, {}, sourceCode)
     },
   }
+}
+
+const hasIslandsNode: Statement = {
+  type: 'ExportNamedDeclaration',
+  specifiers: [],
+  declaration: {
+    type: 'VariableDeclaration',
+    declarations: [
+      {
+        type: 'VariableDeclarator',
+        id: { type: 'Identifier', name: IMPORTING_ISLANDS_ID },
+        init: { type: 'BooleanLiteral', value: true },
+      },
+    ],
+    kind: 'const',
+  },
 }
