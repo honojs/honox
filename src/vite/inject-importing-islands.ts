23d14473f60a4eac2257f1eee253364e7d166fa7
Bruce Harrison <leeharrison1984@gmail.com>
2024-02-28 12:57:58 -0600

    skip exists call

--- src/vite/inject-importing-islands.ts
+++ src/vite/inject-importing-islands.ts
@@ -1,26 +1,51 @@
-import dependencyTree from 'dependency-tree'
+import { readFile } from 'fs/promises'
+import path from 'path'
 import MagicString from 'magic-string'
+import precinct from 'precinct'
 import { normalizePath, type Plugin } from 'vite'
-import { IMPORTING_ISLANDS_ID } from '../constants.js'
+import { IMPORTING_ISLANDS_ID } from '../constants'
 
-export function injectImportingIslands(): Plugin {
-  const visited = {}
+export async function injectImportingIslands(): Promise<Plugin> {
   const isIslandRegex = new RegExp(/\/islands\//)
   const routesRegex = new RegExp(/routes\/.*\.[t|j]sx$/)
+  const cache: Record<string, string> = {}
+
+  const walkDependencyTree: (
+    baseFile: string,
+    dependencyFile?: string
+  ) => Promise<string[]> = async (baseFile: string, dependencyFile?: string) => {
+    const depPath = dependencyFile
+      ? path.join(path.dirname(baseFile), dependencyFile) + '.tsx' //TODO: This only includes tsx files, how to also include JSX?
+      : baseFile
+    const deps = [depPath]
+
+    try {
+      if (!cache[depPath]) {
+        cache[depPath] = (await readFile(depPath, { flag: '' })).toString()
+      }
+
+      const currentFileDeps = precinct(cache[depPath]) as string[]
+
+      const childDeps = await Promise.all(
+        currentFileDeps.map(async (x) => await walkDependencyTree(depPath, x))
+      )
+      deps.push(...childDeps.flat())
+      return deps
+    } catch (err) {
+      // file does not exist or is a directory
+      return deps
+    }
+  }
 
   return {
     name: 'inject-importing-islands',
-    transform(sourceCode, id) {
+    async transform(sourceCode, id) {
       if (!routesRegex.test(id)) {
         return
       }
 
-      const hasIslandsImport = dependencyTree
-        .toList({
-          filename: id,
-          directory: '.',
-          visited,
-        })
+      const hasIslandsImport = (await walkDependencyTree(id))
+        .flat()
         .some((x) => isIslandRegex.test(normalizePath(x)))
 
       if (!hasIslandsImport) {
