f9471c15108053ca9721eb84a3f98beaba673737
Yusuke Wada <yusuke@kamawada.com>
2024-05-10 11:31:32 +0900

    test: fixed type errors (#175)

--- mocks/api/routes/about/[name].ts
+++ mocks/api/routes/about/[name].ts
@@ -3,16 +3,14 @@ import { Hono } from 'hono'
 const app = new Hono()
 
 app.get('/', (c) => {
-  // @ts-expect-error It throws a type error with hono@4.0.3
-  const name = c.req.param('name')
+  const name = c.req.param<'/:name'>('name')
   return c.json({
     path: `/about/${name}`,
   })
 })
 
 app.get('/address', (c) => {
-  // @ts-expect-error It throws a type error with hono@4.0.3
-  const name = c.req.param('name')
+  const name = c.req.param<'/:name'>('name')
   return c.json({
     path: `/about/${name}/address`,
   })
