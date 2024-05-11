7201b4aaa828f65264c47b2678011eabdffb4cff
Yusuke Wada <yusuke@kamawada.com>
2024-05-11 18:32:27 +0900

    import only `.tsx` at the client

--- src/client/client.ts
+++ src/client/client.ts
@@ -37,9 +37,9 @@ export type ClientOptions = {
 
 export const createClient = async (options?: ClientOptions) => {
   const FILES = options?.ISLAND_FILES ?? {
-    ...import.meta.glob('/app/islands/**/[a-zA-Z0-9-]+.(tsx|ts)'),
-    ...import.meta.glob('/app/**/_[a-zA-Z0-9-]+.island.(tsx|ts)'),
-    ...import.meta.glob('/app/**/$[a-zA-Z0-9-]+.(tsx|ts)'),
+    ...import.meta.glob('/app/islands/**/[a-zA-Z0-9-]+.tsx'),
+    ...import.meta.glob('/app/**/_[a-zA-Z0-9-]+.island.tsx'),
+    ...import.meta.glob('/app/**/$[a-zA-Z0-9-]+.tsx'),
   }
 
   const hydrateComponent: HydrateComponent = async (document) => {
