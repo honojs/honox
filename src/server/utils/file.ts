5ee4dcddfb3a168f8de49726f7d469fcbb96df51
Rokas Muningis <28229273+muningis@users.noreply.github.com>
2025-03-30 20:25:45 +0300

    feat: remove () grouping from filepath when creating route path

--- src/server/utils/file.ts
+++ src/server/utils/file.ts
@@ -5,6 +5,7 @@ export const filePathToPath = (filePath: string) => {
     .replace(/^\/?index$/, '/') // `/index`
     .replace(/\/index$/, '') // `/about/index`
     .replace(/\[\.{3}.+\]/, '*')
+    .replace(/\((.+?)\)/g, '')
     .replace(/\[(.+?)\]/g, ':$1')
   return /^\//.test(filePath) ? filePath : '/' + filePath
 }
