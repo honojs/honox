24f19e85be8922a2f36f38c171f018ff4ce57be1
3w36zj6 <52315048+3w36zj6@users.noreply.github.com>
2025-10-30 09:50:26 +0900

    fix: prevent EBUSY by avoiding absolute paths in watcher

--- src/vite/restart-on-add-unlink.ts
+++ src/vite/restart-on-add-unlink.ts
@@ -4,7 +4,7 @@ export function restartOnAddUnlink(): Plugin {
   return {
     name: 'restart-on-add-unlink',
     configureServer(server) {
-      server.watcher.add('/app/**')
+      server.watcher.add('./app/**')
       server.watcher.on('add', async () => {
         await server.restart()
       })
