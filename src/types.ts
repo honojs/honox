573a6ea8e5912687bb3d3221043b998fa6adf17d
Yusuke Wada <yusuke@kamawada.com>
2026-05-28 20:11:43 +0900

    format

--- src/types.ts
+++ src/types.ts
@@ -3,9 +3,7 @@
 /** JSX */
 export type CreateElement<E = Node> = (type: any, props: any) => E | Promise<E>
 export type Hydrate<E = Node> = (children: E, parent: Element) => void | Promise<void>
-export type CreateChildren<E = Node> = (
-  childNodes: NodeListOf<ChildNode>
-) => E[] | Promise<E[]>
+export type CreateChildren<E = Node> = (childNodes: NodeListOf<ChildNode>) => E[] | Promise<E[]>
 export type HydrateComponent = (doc: {
   querySelectorAll: typeof document.querySelectorAll
 }) => Promise<void>
